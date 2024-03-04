304c1bd978e7c05b384934e766c4065202c5516a
emma jouveneaux <emma.jouveneaux@epfedu.fr>
2024-03-04 15:06:24 +0100

    avancées sur commandes

--- src/js/commandes.js
+++ src/js/commandes.js
@@ -17,14 +17,7 @@ this.load.image('livre', 'src/assets/book.png');
     fct.doAlsoNothing();
     this.add.image(0, 0, "livre").setOrigin(0).setDepth(0).setDisplaySize(800, 600)
 
-    // Create a new HTML document
-var newDoc = document.implementation.createHTMLDocument("New Document");
 
-// Clear the body content
-newDoc.body.innerHTML = '';
-
-// Append the new document to the current window
-document.documentElement.replaceWith(newDoc.documentElement);
 
   }
 
