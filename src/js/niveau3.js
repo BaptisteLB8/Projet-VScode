113d49408012dcc53e77a87062527dc6149a6bdf
emma jouveneaux <emma.jouveneaux@epfedu.fr>
2024-03-05 10:29:09 +0100

    le perso marche pas

--- src/js/niveau3.js
+++ src/js/niveau3.js
@@ -16,7 +16,7 @@ export default class niveau3 extends Phaser.Scene {
     this.load.image("tuilesdejeu3","src/assets/nuit.jpg")
     this.load.tilemapTiledJSON("carte","src/assets/map_niveau3.tmj");
 
-    this.load.spritesheet("img_perso", "src/assets/dude.png", {
+    this.load.spritesheet("img_perso", "src/assets/farmer.png", {
       frameWidth: 32,
       frameHeight: 48
     }); 
@@ -35,21 +35,21 @@ export default class niveau3 extends Phaser.Scene {
 
     this.anims.create({
       key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
-      frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+      frames: this.anims.generateFrameNumbers("img_perso", { start: 1, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3
       frameRate: 10, // vitesse de défilement des frames
       repeat: -1 // nombre de répétitions de l'animation. -1 = infini
     }); 
   
     this.anims.create({
       key: "anim_tourne_droite", // key est le nom de l'animation : doit etre unique poru la scene.
-      frames: this.anims.generateFrameNumbers("img_perso", { start:5 , end: 8 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+      frames: this.anims.generateFrameNumbers("img_perso", { start:6 , end: 8 }), // on prend toutes les frames de img perso numerotées de 0 à 3
       frameRate: 10, // vitesse de défilement des frames
       repeat: -1 // nombre de répétitions de l'animation. -1 = infini
     }); 
   
     this.anims.create({
       key: "anim_face",
-      frames: [{ key: "img_perso", frame: 4 }],
+      frames: [{ key: "img_perso", frame: 2 }],
       frameRate: 20
     }); 
   
