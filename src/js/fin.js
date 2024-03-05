886a5059bcdc4b04855b98c85d23611581d3f99c
emma jouveneaux <emma.jouveneaux@epfedu.fr>
2024-03-05 14:51:17 +0100

    ajout de la fenetre de defaite

--- src/js/fin.js
+++ src/js/fin.js
@@ -9,17 +9,23 @@ export default class fin extends Phaser.Scene {
   }
   preload() {
 this.load.image('over', 'src/assets/GameOver.png'); 
-this.load.image('retour1', 'src/assets/retour.png'); 
+this.load.image('retry', 'src/assets/retry.png'); 
+this.load.image('quit', 'src/assets/quit.png');
 
   }
 
   create() {
     fct.doNothing();
     fct.doAlsoNothing();
-    this.add.image(0, 0, "livre").setOrigin(0).setDepth(0).setDisplaySize(800, 600);
-    var bouton_return = this.add.image (650,565,"retour1").setDepth(1).setDisplaySize(50, 50);
-    bouton_return.setInteractive();
-    bouton_return.on("pointerup", () => {
+    this.add.image(50, 50, "over").setOrigin(0).setDepth(0).setDisplaySize(700, 500);
+    var bouton_retry = this.add.image (500,0,"retry").setDepth(1).setDisplaySize(50, 50);
+    var bouton_quit = this.add.image (200,0,"quit").setDepth(1).setDisplaySize(50, 50);
+    bouton_retry.setInteractive();
+    bouton_retry.on("pointerup", () => {
+      this.scene.start("selection");
+    });
+    bouton_quit.setInteractive();
+    bouton_quit.on("pointerup", () => {
       this.scene.start("selection");
     });
     
