46139e26c91605b51be0cee6f470c30522b44de2
emma jouveneaux <emma.jouveneaux@epfedu.fr>
2024-03-06 14:13:59 +0100

    probleme de caillou reparé

--- src/js/niveau1.js
+++ src/js/niveau1.js
@@ -182,6 +182,22 @@ this.physics.add.collider(this.groupeBullets, mzelda, this.collisionFlecheMzelda
     if (this.gameOver) {
       this.vie--; // Décrémentez le nombre de vies
       this.text.setText("Il vous reste " + this.vie + " vie(s)"); // Mettez à jour le texte affichant le nombre de vies
+      if (this.levier.flipX = true){
+        this.levier.active = false;
+        this.levier.flipX = false; // on tourne l'image du levier
+        this.tween_mouvement.stop();  // on relance le tween
+        this.bloquage=true;
+        var caillou_mobile = this.physics.add.sprite(
+          993,
+          107,
+          "caillou"
+        ); 
+        caillou_mobile.setScale(0.15);
+        caillou_mobile.body.allowGravity = false;
+        caillou_mobile.body.immovable = true; }
+
+
+
       if (this.vie <= 0) { // Si le joueur n'a plus qu'une seule vie
         this.scene.start("fin"); // Redirigez vers la scène de fin de jeu
 
